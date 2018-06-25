5f0cf49b7962492e7a0a6408dbdb3bf2724c84b3
Kelsey Fecho <kfecho30@gmail.com>
2018-06-25 19:33:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ function showRepositories(event, data) {
   document.getElementById("repositories").innerHTML = repoList
 }
 
+function getCommits(el) {
+  const name = el.dataset.repo
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showCommits)
+  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
+  req.send()
+}
+
 function getRepositories() {
   const req = new XMLHttpRequest()
   req.addEventListener("load", showRepositories);
