31a36a41b6543e22f9ef5cb32c71393c01373027
Kelsey Fecho <kfecho30@gmail.com>
2018-06-25 19:24:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function getRepositories() {
   const req = new XMLHttpRequest()
-  req.open("GET", 'https://api.github.com/users/kfecho/repos')
+  req.addEventListener("load", showRepositories);
+  req.open("GET", 'https://api.github.com/users/kfecho30/repos')
   req.send()
 }
