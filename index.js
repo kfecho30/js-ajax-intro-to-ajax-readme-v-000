6681919017e342ad9e2c65a21ec6bef9531225ce
Kelsey Fecho <kfecho30@gmail.com>
2018-06-25 19:30:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 function showRepositories(event, data) {
-  //this is set to the XMLHttpRequest object that fired the event
-  const repos = JSON.parse(this.responseText)
+  var repos = JSON.parse(this.responseText)
   console.log(repos)
-  const reposList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`
-  
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
 
